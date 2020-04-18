4520a5d8568742a7e94a993085d3ad4238baef69
Maha Nagarjuna Angalakuditi <maha.nagarjuna@gmail.com>
2020-04-18 13:22:14 +0530

    18Apr2020

---
+++ 2020-hakerRank/com/angma02/ds/trees/Node.java
@@ -0,0 +1,17 @@
+package com.angma02.ds.trees;
+
+public class Node {
+
+    int value;
+    Node left;
+    Node right;
+
+    public Node(int value) {
+        this.value = value;
+    }
+
+    public void insert(int val) {
+
+    }
+
+}
